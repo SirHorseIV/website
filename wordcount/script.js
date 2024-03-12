8ee14c2b96fdd4b05eaf86546802dfd02f621504
SirHorseIV <78271552+SirHorseIV@users.noreply.github.com>
2024-03-12 21:17:38 +0000

    Add files via upload

---
+++ wordcount/script.js
@@ -0,0 +1,24 @@
+let textArea = document.getElementById("input");
+let wordCount = document.getElementById("wordCount");
+let wordCountWithoutQuotes = document.getElementById("wordCountWithoutQuotes");
+
+updateWordCount();
+
+function updateWordCount() {
+    let text = textArea.value;
+    const quoteArray = text.split(/["“”]/);
+    let total = 0;
+    for (let i = 0; i < quoteArray.length; i += 2) {
+        total += getWordCount(quoteArray[i]);
+    }
+    wordCount.innerHTML = `Word count: ${getWordCount(text)}`;
+    wordCountWithoutQuotes.innerHTML = `Word count without quotations: ${total}`;
+}
+
+function getWordCount(words) {
+    const wordArray = words.split(" ");
+    for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i] == "" || wordArray[i] == "-" || wordArray[i] == "–" || wordArray[i] == ",") { wordArray.splice(i, 1); i--; }
+    }
+    return wordArray.length;
+}
